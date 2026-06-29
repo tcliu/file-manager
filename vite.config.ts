5f0f82aa3fa544d4da145c3991e55d8152405ed8
tcliu <tcliu@live.hk>
2026-06-29 22:02:40 +0800

    feat: add @vitejs/plugin-basic-ssl for HTTPS support in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss()]
+  plugins: [basicSsl(), sveltekit(), tailwindcss()],
+  preview: {
+    allowedHosts: ['.duckdns.org'],
+    https: {}
+  }
 });
