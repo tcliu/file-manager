bd366a66c9b25c79c6959d17483fd7db979954a8
tcliu <tcliu@live.hk>
2026-06-30 11:35:20 +0800

    feat: add external dependency 'sharp' to Vercel adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({ runtime: 'nodejs24.x' })
+    adapter: adapter({ runtime: 'nodejs24.x', external: ['sharp'] })
   }
 };
 
